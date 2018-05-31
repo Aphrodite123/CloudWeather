20222d15415fa88a528142c26892525c3427762a
wanglin <wanglin@jiqid.com>
2018-05-31 11:10:53 +0800

    新建项目

---
+++ CloudWeather/app/src/main/java/com/aphrodite/cloudweather/CloudWeatherActivity.java
@@ -0,0 +1,13 @@
+package com.aphrodite.cloudweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CloudWeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cloud_weather);
+    }
+}
